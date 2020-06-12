7e12d34fbaa1a08cdcae3c7fd7dcf84b09495409
vepriya <46642471+vepriya@users.noreply.github.com>
2020-06-12 17:23:11 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+
+
+# initialize the list of class label names present in dataset
+CLASSES = ["Bread", "Dairy product", "Dessert", "Egg", "Fried food",
+	"Meat", "Noodles/Pasta", "Rice", "Seafood", "Soup",
+	"Vegetable/Fruit"]
